8ea1049b8fc34113d453124286a8f7d02020e4e2
nugets241 <sobrepenaej@yahoo.com>
2026-01-15 16:22:47 +0200

    Add PUT route to unlike a post with user validation

--- routes/api/post.js
+++ routes/api/post.js
@@ -132,4 +132,32 @@ router.put('/:id/like', auth, async (req, res) => {
 	}
 });
 
+// @route   PUT api/posts/:id/unlike
+// @desc    Unlike a post
+// @access  Private
+router.put('/:id/unlike', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
+		// Check if user hasn't liked the post
+		if (!post.likes.includes(req.user.id)) {
+			return res.status(400).json({ msg: 'You have not liked this post' });
+		}
+
+		const removeIndex = post.likes.indexOf(req.user.id);
+		post.likes.splice(removeIndex, 1);
+		await post.save();
+		res.json(post.likes);
+	} catch (error) {
+		console.error(error.message);
+		if (error.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+		res.status(500).send('Server error');
+	}
+});
+
 export default router;
