2c21b4ec145b18c2e143a44b32fa769038bcc21c
nugets241 <sobrepenaej@yahoo.com>
2026-01-15 11:45:12 +0200

    Add DELETE route to remove user profile and account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -126,4 +126,20 @@ router.get('/user/:user_id', async (_req, res) => {
 	}
 });
 
+// @route   DELETE api/profile/
+// @desc    Delete profile, users and posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+	try {
+		// Remove profile
+		await Profile.findOneAndDelete({ user: req.user.id });
+		// Remove user
+		await User.findOneAndDelete({ _id: req.user.id });
+		res.json({ msg: 'User deleted' });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
+
 export default router;
