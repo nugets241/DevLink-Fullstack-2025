4c605d2e6a27b1be235592bc1af90a74c63ad31a
nugets241 <sobrepenaej@yahoo.com>
2026-01-15 11:45:40 +0200

    Add PUT route to add experience to user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -142,4 +142,53 @@ router.delete('/', auth, async (req, res) => {
 	}
 });
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+	'/experience',
+	[
+		auth,
+		[
+			check('title', 'Title is required').not().isEmpty(),
+			check('company', 'Company is required').not().isEmpty(),
+			check('from', 'From date is required').not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { title, company, location, from, to, current, description } =
+			req.body;
+
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+			if (!profile) {
+				return res.status(404).json({ msg: 'Profile not found' });
+			}
+
+			profile.experience.unshift(newExp);
+			await profile.save();
+
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
+
 export default router;
