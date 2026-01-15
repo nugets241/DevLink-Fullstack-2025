1c239ca4181077abde5dfd5502f4fcfb16194f9e
nugets241 <sobrepenaej@yahoo.com>
2026-01-15 11:46:23 +0200

    Add PUT route to add education to user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -220,4 +220,54 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 	}
 });
 
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put(
+	'/education',
+	[
+		auth,
+		[
+			check('school', 'School is required').not().isEmpty(),
+			check('degree', 'Degree is required').not().isEmpty(),
+			check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+			check('from', 'From date is required').not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { school, degree, fieldofstudy, from, to, current, description } =
+			req.body;
+
+		const newEdu = {
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+			if (!profile) {
+				return res.status(404).json({ msg: 'Profile not found' });
+			}
+
+			profile.education.unshift(newEdu);
+			await profile.save();
+
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
+
 export default router;
