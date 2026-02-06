91759cb123d1eaf5ce553e4d821dde5dca5b2d1d
nugets241 <sobrepenaej@yahoo.com>
2026-02-06 02:19:19 +0200

    feat: add Button component with variant support for primary and secondary styles

---
+++ client/src/components/common/Button.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import clsx from 'clsx';
+
+interface IButton extends React.ComponentPropsWithoutRef<'button'> {
+	/**
+	 * Select the visual variant for the button. Default is "primary"
+	 *
+	 * @default "primary"
+	 */
+	variant?: 'primary' | 'secondary';
+}
+
+/**
+ * Standard button component, which is styled according to M-Files Design System.
+ */
+function Button({
+	type = 'button',
+	variant = 'primary',
+	className,
+	children,
+	...props
+}: React.PropsWithChildren<IButton>) {
+	// Concatenate class names.
+	const classNames = clsx(
+		'button',
+		{
+			// Primary button
+			['button-primary']: variant === 'primary',
+
+			// Secondary button
+			['button-secondary']: variant === 'secondary',
+		},
+		className,
+	);
+
+	return (
+		<button type={type} className={classNames} {...props}>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
