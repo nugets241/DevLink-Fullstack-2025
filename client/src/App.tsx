48bd6a6fd19baa0e0388b1619fc85348b965a359
nugets241 <sobrepenaej@yahoo.com>
2026-02-06 23:30:54 +0200

    style: implement routing in App component using Routes and Route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import Landing from './components/layout/Landing';
 import Register from './pages/Register';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
-		<>
-			<Landing />
-			{/* <Register /> */}
-		</>
+		<Routes>
+			<Route path="/" element={<Landing />} />
+			<Route path="/register" element={<Register />} />
+			<Route path="*" element={<Navigate to="/" replace />} />
+		</Routes>
 	);
 }
 
