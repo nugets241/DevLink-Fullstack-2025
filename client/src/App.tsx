7e70237452c6f30104a499ea4f5d9725e4445668
nugets241 <sobrepenaej@yahoo.com>
2026-02-06 22:29:46 +0200

    style: import Register component and add layout/register styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import Landing from './components/layout/Landing';
+import Register from './pages/Register';
 
 function App() {
 	return (
 		<>
 			<Landing />
+			{/* <Register /> */}
 		</>
 	);
 }
