724d17f73c1678860e68d5a338f65eed8b0fbd1b
nugets241 <sobrepenaej@yahoo.com>
2026-02-23 16:03:24 +0200

    style: update App component to conditionally render Navbar and Home based on authentication token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,22 @@
 import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
 import Register from './pages/Register';
+import Home from './pages/Home';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { useAppSelector } from './store/hooks';
 
 function App() {
+	const { token } = useAppSelector((state) => state.auth);
+
 	return (
-		<Routes>
-			<Route path="/" element={<Landing />} />
-			<Route path="/register" element={<Register />} />
-			<Route path="*" element={<Navigate to="/" replace />} />
-		</Routes>
+		<>
+			{token && <Navbar />}
+			<Routes>
+				<Route path="/" element={token ? <Home /> : <Landing />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</>
 	);
 }
 
