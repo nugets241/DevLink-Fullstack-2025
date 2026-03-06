44441ff9e8ccec09d18c1f2ff943956e32e98010
nugets241 <sobrepenaej@yahoo.com>
2026-03-06 14:15:43 +0200

    feat: add posts reducer to store configuration

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import profileReducer from './slices/profileSlice';
+import postsReducer from './slices/postsSlice';
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
 		profile: profileReducer,
+		posts: postsReducer,
 	},
 });
 
